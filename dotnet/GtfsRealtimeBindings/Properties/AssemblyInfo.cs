2ae5fad0f465d40ec1d608b72290bba9a8ad0125
Kevin Hobson <kevin.hobson@smgov.net>
2023-07-15 08:15:30 -0700

    upgrade from framework to core

--- dotnet/GtfsRealtimeBindings/Properties/AssemblyInfo.cs
+++ dotnet/GtfsRealtimeBindings/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GtfsRealtimeBindings")]
-[assembly: AssemblyDescription("Provides .NET classes generated from the GTFS-realtime Protocol Buffer specification.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MobilityData")]
-[assembly: AssemblyProduct("GtfsRealtimeBindings")]
-[assembly: AssemblyCopyright("Copyright ©  2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("278a7ee5-5f39-4420-82a9-c3462dafb326")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4.0")]
-[assembly: AssemblyFileVersion("0.0.4.0")]
